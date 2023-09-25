8fbd2df5eaab27869a7d278b63dc973b84379a8e
Romailson <antoniofernandes1999@outlook.com>
2023-09-25 14:45:57 -0300

    continuação do menu

--- academia/Menu.java
+++ academia/Menu.java
@@ -25,6 +25,62 @@ public class Menu {
             System.out.print("Escolha uma opção: ");
             escolha = lerOpcao();
 
-        }
+            switch(escolha){
+                case 1:
+                  AcademiaApp.cadrastrarAluno();
+                  break;
+                case 2:
+                  AcademiaApp.listarAlunos();
+                  break;
+                case 3:
+                  AcademiaApp.editarAluno();
+                  break;
+                case 4:
+                  AcademiaApp.excluirAluno();
+                  break;
+                case 5:
+                  AcademiaApp.cadastrarInstrutor();
+                  break;
+                case 6:
+                  AcademiaApp.listarInstrutores();
+                    break;
+                case 7:
+                    AcademiaApp.editarInstrutor();
+                    break;
+                case 8:
+                    AcademiaApp.excluirInstrutor();
+                    break;
+                case 9:
+                    AcademiaApp.tornarAlunoMembro();
+                    break;
+                case 10: 
+                    AcademiaApp.removerAlunoMembro();
+                    break;
+                case 11:
+                    AcademiaApp.listarAlunosMembros();
+                    break;
+                case 12:
+                AcademiaApp.buscarPorId();
+                    break;
+                case 13:
+                    AcademiaApp.obterTreino();
+                    break;
+                case 14:
+                    System.out.println("Saindo do programa...");
+                    return;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }   
     }
-}
+}  
+
+
+    
+
+
+
+
+        
+    
+

7981403c8299f56ee12b3178456387c80ad6e51b
Maria Eduarda <mariaedufs13@gmail.com>
2023-09-25 14:42:54 -0300

    continuando

--- academia/AcademiaApp.java
+++ academia/AcademiaApp.java
@@ -10,4 +10,23 @@ public class AcademiaApp {
     alunoMembros = new ArrayList<>(); // Lista para armazenar alunos membros
     private static Treino treino = new Treino();
     private static Scanner scanner = new
+Scanner(System.in);
+  private static AtomicInteger alunoldGenerator = new AtomicInteger(1);
+  private static AtomicInteger instrutorldGenerator = new AtomicInteger(1);
+
+
+  public static void cadastrarAluno(){
+    System.out.println("\nCadastrar Aluno:");
+    System.out.println("Nome do aluno: ");
+    String nome = scanner.nextLine();
+
+    // Verificar se o nome contem apenas letras e espacos
+    if (!nome.matches("^[a-zA-Z\\s]+$")) {
+        System.out.println("O nome do aluno nao pode conter caracteres especiais.");
+        return;
+
+    }
+     
+    // Coloque a primeira letra do nome em maiuscula e o restante minusculas
+  }
 }

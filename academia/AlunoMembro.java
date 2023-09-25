09c879606a19f07123cf678c59c81dba68381fd4
Emersonfgerd <ermesonigor2002@gmail.com>
2023-09-25 10:25:18 -0300

    Aluno

--- academia/AlunoMembro.java
+++ academia/AlunoMembro.java
@@ -1,15 +1,11 @@
 public class AlunoMembro extends Aluno {
-    public AlunoMembro(int id, String nome, int idade, boolean problemaSaude, String sexo, String tipoPagamento){
-        super(id, nome, idade, sexo, problemaSaude, tipoPagamento, problemaSaude);
-    }
 
-    //método para remover o status de membro do aluno
-    public void removerMembro(){
-        setMembro(false);
+    public AlunoMembro(int id, String nome, int idade, boolean problemaSaude, String sexo, String tipoPagamento) {
+        super(id, nome, idade, problemaSaude, sexo, tipoPagamento);
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return super.toString();
     }
 }

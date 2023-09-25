da09049abd014b4986e443253f41d4df1055ec06
Emersonfgerd <ermesonigor2002@gmail.com>
2023-09-25 10:23:56 -0300

    classe aluno

--- academia/Aluno.java
+++ academia/Aluno.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Aluno {
     private int id;
     private String nome;
@@ -7,85 +9,70 @@ public class Aluno {
     private String tipoPagamento;
     private boolean membro;
 
-    
-    public Aluno(int id, String nome, int idade, String sexo, boolean problemaSaude, String tipoPagamento, boolean membro) {
+    public Aluno(int id, String nome, int idade, boolean problemaSaude, String sexo, String tipoPagamento) {
         this.id = id;
         this.nome = nome;
         this.idade = idade;
         this.sexo = sexo;
         this.problemaSaude = problemaSaude;
         this.tipoPagamento = tipoPagamento;
-        this.membro = false; //Por padrão o aluno não é membro.
-    }
-    
-    public int getId() {
-        return id;
+        this.membro = false;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public int getId() {
+        return id;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public String getSexo() {
+        return sexo;
     }
 
     public int getIdade() {
         return idade;
     }
 
-    public String getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
     public boolean isProblemaSaude() {
         return problemaSaude;
-    }
-
-    public void setProblemaSaude(boolean problemaSaude) {
-        this.problemaSaude = problemaSaude;
     }
 
     public String getTipoPagamento() {
         return tipoPagamento;
     }
 
-    public void setTipoPagamento(String tipoPagamento) {
-        this.tipoPagamento = tipoPagamento;
-    }
-
     public boolean isMembro() {
         return membro;
-    } 
-
-    public void setMembro(boolean membro) {
-        this.membro = membro;
     }
 
-    public void tornarMembro(){
+    public void tornarMembro() {
+        this.membro = true;
+    }
+
+    public void removerMembro() {
         this.membro = false;
     }
 
-    public void removerMembro(){
-        this.membro = false;
-    }
-    public void editarAluno(String nome, int idade, boolean problemaSaude, String tipoPagamento){
+    public void editarAluno(String nome, int idade, boolean problemaSaude, String tipoPagamento) {
         this.nome = nome;
         this.idade = idade;
         this.problemaSaude = problemaSaude;
         this.tipoPagamento = tipoPagamento;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return id == aluno.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
     @Override

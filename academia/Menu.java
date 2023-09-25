d21a09ebeb5f2eb1dd7e34b40aed44310a84d2da
Romailson <antoniofernandes1999@outlook.com>
2023-09-25 14:22:58 -0300

    inicio do menu

--- academia/Menu.java
+++ academia/Menu.java
@@ -1,3 +1,30 @@
+package academia;
+
+import java.util.Scanner;
 public class Menu {
-    //menu princilpal
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[]args){
+        int escolha;
+        while(true){
+            System.out.println("\nMenu:");
+            System.out.println("1. Cadrastrar Aluno");
+            System.out.println("2. Listar Alunos");
+            System.out.println("3. Editar Aluno");
+            System.out.println("4. Excluir Aluno");
+            System.out.println("5. Cadrastrar Instrutor");
+            System.out.println("6. Listar Instrutor");
+            System.out.println("7. Editar Instrutor");
+            System.out.println("8. Excluir Instrutor");
+            System.out.println("9. Tornar Aluno Membro");
+            System.out.println("10. Excluir um Membro");
+            //Alterada para opção 10
+            System.out.println("11. Listar Alunos Membros");
+            System.out.println("12. Buscar por ID");
+            System.out.println("13. Obter Treino");
+            System.out.println("14. Sair do Programa");
+            System.out.print("Escolha uma opção: ");
+            escolha = lerOpcao();
+
+        }
+    }
 }

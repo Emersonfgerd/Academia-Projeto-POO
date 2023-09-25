4d7374508bec7f30646ea4876ffee4292cb76f12
Emersonfgerd <ermesonigor2002@gmail.com>
2023-09-25 10:42:56 -0300

    Instrutor

--- academia/Instrutor.java
+++ academia/Instrutor.java
@@ -1,3 +1,48 @@
+import java.util.Objects;
+
 public class Instrutor {
+    private int id;
+    private String nome;
+    private String turnoTrabalho;
+
+    public Instrutor(int id, String nome, String turnoTrabalho) {
+        this.id = id;
+        this.nome = nome;
+        this.turnoTrabalho = turnoTrabalho;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTurnoTrabalho() {
+        return turnoTrabalho;
+    }
+
+    public void editarInstrutor(String novoNome, String novoTurnoTrabalho){
+        this.nome = novoNome;
+        this.turnoTrabalho = novoTurnoTrabalho;
+    }
     
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Instrutor instrutor = (Instrutor) o;
+        return id == instrutor.id;
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + "\nNome: " + nome + "\n Turno de Trabalho: " + turnoTrabalho;
+    }    
 }
